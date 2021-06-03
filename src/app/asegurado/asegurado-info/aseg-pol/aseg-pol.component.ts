a7cee3d4df75d2661006148738f90b13dde7b9eb
Lucas Di Rico <lucasdirico.ros@gmail.com>
2021-06-03 04:49:52 -0300

    agregada tabla de polizas aseg-pol.component

--- src/app/asegurado/asegurado-info/aseg-pol/aseg-pol.component.ts
+++ src/app/asegurado/asegurado-info/aseg-pol/aseg-pol.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AsegPolComponent implements OnInit {
 
+  displayedColumns: string[] = ['position', 'cover', 'sumaaseg', 'detail', 'del'];
+
+  datos: any = [
+    {"id": "01", "cover": "vida", "sumaaseg": "$ 400.000"},
+    {"id": "02", "cover": "retiro", "sumaaseg": "$ 1.000.000"},
+    {"id": "03", "cover": "accidentes", "sumaaseg": "$ 500.000"},
+    {"id": "04", "cover": "vida plus", "sumaaseg": "$ 300.000"}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  deletePoliza(id: any){
+    console.log("eliminado "+id);
+  }
+
+  updatePoliza(data: any){
+    console.log("modificar "+data.id);
   }
 
 }
--- src/app/material-blocks/material-blocks.module.ts
+++ src/app/material-blocks/material-blocks.module.ts
@@ -4,6 +4,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card'; 
 import {MatDividerModule} from '@angular/material/divider'; 
+import {MatTableModule} from '@angular/material/table';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -12,13 +14,17 @@ import {MatDividerModule} from '@angular/material/divider';
      MatToolbarModule,
      MatButtonModule,
      MatCardModule,
-     MatDividerModule
+     MatDividerModule,
+     MatTableModule,
+     MatIconModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
-    MatDividerModule
+    MatDividerModule,
+    MatTableModule,
+    MatIconModule
   ]
 })
 export class MaterialBlocksModule { }
