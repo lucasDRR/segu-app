4b13f5ef8a8937a8d76692c232b1898abd7d0745
Lucas Di Rico <lucasdirico.ros@gmail.com>
2021-06-07 04:19:47 -0300

    modificado home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SeguHomeComponent } from './segu-home/segu-home.component';
 
 const routes: Routes = [
+  {path: '', component: SeguHomeComponent},
   { path: 'asegurado', 
     loadChildren: () => import('./asegurado/asegurado.module').then(m => m.AseguradoModule) }
 ];
