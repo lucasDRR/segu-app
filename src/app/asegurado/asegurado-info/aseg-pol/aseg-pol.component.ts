03b8d346b900ccd6a5ed4e7a27c6b9ef7005cfa8
Lucas Di Rico <lucasdirico.ros@gmail.com>
2021-06-07 04:08:33 -0300

    fake login v2

--- src/app/asegurado/asegurado-info/aseg-pol/aseg-pol.component.ts
+++ src/app/asegurado/asegurado-info/aseg-pol/aseg-pol.component.ts
@@ -11,26 +11,24 @@ export class AsegPolComponent implements OnInit {
   displayedColumns: string[] = ['position', 'cover', 'sumaaseg', 'detail', 'del'];
 
   datos: any = [
-    {"id": "01", "cover": "vida", "sumaaseg": "$ 400.000"},
-    {"id": "02", "cover": "retiro", "sumaaseg": "$ 1.000.000"},
-    {"id": "03", "cover": "accidentes", "sumaaseg": "$ 500.000"},
-    {"id": "04", "cover": "vida plus", "sumaaseg": "$ 300.000"}
+    { "id": "01", "cover": "vida", "sumaaseg": "$ 400.000" },
+    { "id": "02", "cover": "retiro", "sumaaseg": "$ 1.000.000" },
+    { "id": "03", "cover": "accidentes", "sumaaseg": "$ 500.000" },
+    { "id": "04", "cover": "vida plus", "sumaaseg": "$ 300.000" }
   ];
 
-  constructor(private router: Router ) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  deletePoliza(id: any){
-    console.log("eliminado "+id);
+  deletePoliza(id: any) {
+    console.log("eliminado " + id);
   }
 
-  updatePoliza(data: any){
-    console.log("modificar "+data.id);
-
-    let idNum =  parseInt(data.id, 10);
-
+  updatePoliza(data: any) {
+    console.log("modificar " + data.id);
+    let idNum = parseInt(data.id, 10);
     this.router.navigate(['asegurado/poliza', idNum]);
   }
 
--- src/app/asegurado/fake-login/fake-login.component.ts
+++ src/app/asegurado/fake-login/fake-login.component.ts
@@ -10,52 +10,63 @@ import { Usuario } from '../usuario';
 })
 export class FakeLoginComponent implements OnInit {
 
-  usuarios : Usuario[] = [
-    { "id": 1, nombre : "Lucas", apellido: "Diaz", dni:"11.222.999"},
-    { "id": 2, nombre : "Marcelo", apellido: "Perez", dni:"11.222.666"},
-    { "id": 3, nombre : "Jose", apellido: "Lopez", dni:"11.222.333"},
+  usuarios: Usuario[] = [
+    { "id": 1, nombre: "Lucas", apellido: "Diaz", dni: "11.222.999" },
+    { "id": 2, nombre: "Marcelo", apellido: "Perez", dni: "11.222.666" },
+    { "id": 3, nombre: "Jose", apellido: "Lopez", dni: "11.222.333" },
 
   ]
   fakelog: FormGroup = new FormGroup({});
-  
+
   usuarioEncontrado: Usuario | undefined;
+
   validUser = false;
-  
-  constructor(private fb:FormBuilder, private router: Router) {}
+
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit(): void {
-    this.fakelog = this.newFakeLogForm();
+    this.fakelog = this.newFakelogForm();
   }
 
-  onLogin(data:any): void {
+  onLogin(data: any): void {
 
-    this.usuarioEncontrado = this.usuarios.find( 
+    /* buscar al asegurado en el arrary de asegurados (fake data)
+    ** por nombre y apellido, en la implementacion real esta accion
+    ** la realiza el back end 
+    */
+
+    this.usuarioEncontrado = this.usuarios.find(
       u => {
-        if( u.nombre === data.nombre && u.apellido === data.apellido){
+        if (u.nombre === data.nombre && u.apellido === data.apellido) {
           return true;
-        }else{
+        } else {
           return false;
         }
-    });
+      });
 
-    if(this.usuarioEncontrado != undefined){
+    if (this.usuarioEncontrado != undefined) {
+
       this.validUser = true;
       let userId: number = this.usuarioEncontrado.id;
       this.router.navigate(['asegurado', userId]);
-      
-    }else{
-      this.validUser = false;
-      this.resetFakeLog();
+
+    } else {
+
+      this.resetFakelog();
     }
-    
+
   }
 
-  private resetFakeLog(){
-    this.fakelog.reset();
-    this.fakelog = this.newFakeLogForm();
+  private resetFakelog(): void {
+
+    this.fakelog.reset({
+      nombre: ' ',
+      apellido: ' '
+    });
+    this.fakelog.updateValueAndValidity();
   }
 
-  private newFakeLogForm(): FormGroup {
+  private newFakelogForm(): FormGroup {
     return this.fb.group({
       nombre: ['', [Validators.required]],
       apellido: ['', [Validators.required]]
@@ -63,3 +74,5 @@ export class FakeLoginComponent implements OnInit {
   }
 
 }
+
+
